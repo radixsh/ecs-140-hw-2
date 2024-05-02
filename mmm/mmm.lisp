7f3ce5ded86a5fe8520f08ccc61a40f50d99a436
Radish <53844660+radixsh@users.noreply.github.com>
2024-05-02 15:19:53 -0700

    Write mmm.lisp

--- mmm/mmm.lisp
+++ mmm/mmm.lisp
@@ -1,7 +1,61 @@
-; You may define helper functions here
+(defun mylength (mylist)
+  (if (null mylist) 
+    0
+    (+ 1 (mylength (cdr mylist)))
+  )
+)
+
+(defun mymax (mylist)
+  (cond
+    ((null mylist)
+      -99999999
+    )
+    ((equal (mylength mylist) 2)
+      (if (> (car mylist) (car (cdr mylist)))
+        (car mylist)
+        (car (cdr mylist))
+      )
+    )
+    ((> (car mylist) (mymax (cdr mylist)))
+      (car mylist)
+    )
+    (T
+      (mymax (cdr mylist))
+    )
+  )
+)
+
+(defun mymin (mylist)
+  (cond
+    ((null mylist)
+      9999999999
+    )
+    ((equal (mylength mylist) 2)
+      (if (< (car mylist) (car (cdr mylist)))
+        (car mylist)
+        (car (cdr mylist))
+      )
+    )
+    ((< (car mylist) (mymin (cdr mylist)))
+      (car mylist)
+    )
+    (T
+      (mymin (cdr mylist))
+    )
+  )
+)
+
+(defun mysum (mylist)
+  (cond
+    ((null mylist) 0)
+    (T (+ (car mylist) (mysum (cdr mylist))))
+  )
+)
+
+(defun myavg (mylist)
+  (/ (mysum mylist) (mylength mylist))
+)
 
 (defun min-mean-max (xs)
-    ;; TODO: Incomplete function
-    ;; The next line should not be in your solution.
-    (list 'incomplete)
+  (list (mymin xs) (myavg xs) (mymax xs))
 )
