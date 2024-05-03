29931e00ef2311d9b7fc894418cd2b2e9f4bfc69
Radish <53844660+radixsh@users.noreply.github.com>
2024-05-02 17:56:58 -0700

    Partially write qsort

--- qsort/qsort.lisp
+++ qsort/qsort.lisp
@@ -1,13 +1,66 @@
-; You may define helper functions here
-
 (defun pivot (n xs)
-  ;; TODO: Incomplete function.
-  ;; The next line should not be in your solution.
-  (list 'incomplete)
+  (print 'lower)
+  (print (lower n xs))
+  (print 'upper)
+  (print (upper n xs))
+  (list (lower n xs) (upper n xs))
+)
+
+(defun lower (n mylist)
+  (cond
+    ((null mylist)
+      nil
+    )
+    ((< (car mylist) n)
+      (append (list (car mylist)) (lower n (cdr mylist)))
+    )
+    (t
+      (lower n (cdr mylist))
+    )
+  )
+)
+
+(defun upper (n mylist)
+  (cond
+    ((null mylist)
+      nil
+    )
+    ((>= (car mylist) n)
+      (append (list (car mylist)) (upper n (cdr mylist)))
+    )
+    (t
+      (upper n (cdr mylist))
+    )
+  )
+)
+
+(defun mylength (mylist)
+  (if (null mylist) 
+    0
+    (+ 1 (mylength (cdr mylist)))
+  )
 )
 
 (defun quicksort (xs)
-  ;; TODO: Incomplete function.
-  ;; The next line should not be in your solution.
-  (list 'incomplete)
+  (print '(quicksort called on))
+  (print xs)
+  (cond 
+    ((or (null xs) (equal (mylength xs) 1))
+      xs)
+    ((equal (mylength xs) 2)
+      (print 'twoelements)
+      (if (< (car xs) (car (cdr xs)))
+        (list (car xs) (car (cdr xs)))
+        (list (car (cdr xs)) (car xs))
+      )
+    )
+    (t
+      (print 'default)
+      (let ((p (car xs)))
+        (print 'callingpivot)
+        (mapcar 'quicksort (pivot p xs))
+      )
+    )
+  )
+  
 )
