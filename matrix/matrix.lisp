5cd88058acf914394d8ef575a51185bce3e72fc8
Radish <53844660+radixsh@users.noreply.github.com>
2024-05-05 15:37:26 -0700

    Begin matrix add and transpose

--- matrix/matrix.lisp
+++ matrix/matrix.lisp
@@ -1,20 +1,82 @@
-; You may define helper functions here
+(defun row-add (first second)
+  (cond
+    ((null first)
+      nil
+    )
+    (t
+      (cons (+ (car first) (car second)) 
+        (row-add (cdr first) (cdr second))
+      )
+    )
+  )
+)
 
-(defun matrix-add (mat1 mat2)
-  ;; TODO: Incomplete function.
-  ;; The next line should not be in your solution.
-  (list 'incomplete)
+(defun matrix-add (first second)
+  (cond
+    ((null first) ; Second is nil iff first is null
+      nil
+    )
+    ; matrix-add (1 2) (3 4)
+    ((atom (car first)) ; Each matrix has only 1 row
+      (row-add first second)
+    )
+    ((null (cdr first)) ; The next row after is empty
+      (matrix-add (car first) (car second))
+    )
+    (t
+      (list (matrix-add (car first) (car second))
+            (matrix-add (cdr first) (cdr second))
+      )
+    )
+  )
+)
+
+; ((1 2) (3 4)) --> (1 3)
+(defun getfirst (listoflists)
+  (cond
+    ((null (car listoflists))
+      nil
+    )
+    ((null (cdr listoflists)) ; look ahead
+      (car (car listoflists))
+    )
+    (t
+      ;(list 
+        (cons (car (car listoflists)) (getfirst (cdr listoflists)))
+        ;(list (cdr (car listoflists)) (cdr (car (cdr listoflists))))
+      ;)
+    )
+  )
+)
+
+(defun getrem (listoflists)
+  (cond
+    ((null (car listoflists))
+      nil
+    )
+    ((null (cdr listoflists))
+      (cdr (car listoflists))
+    )
+    (t
+      (cons (cdr (car listoflists)) (getrem (cdr listoflists)))
+    )
+  )
 )
 
 (defun matrix-transpose (mat)
-  ;; TODO: Incomplete function.
-  ;; The next line should not be in your solution.
-  (list 'incomplete)
+  (cond
+    ((null (car mat))
+      nil
+    )
+    (t
+      (cons
+        (getfirst mat)
+        (append (matrix-transpose (getrem mat)))
+      )
+    )
+  )
 )
 
 (defun matrix-multiply (mat1 mat2)
-  ;; TODO: Incomplete function.
-  ;; The next line should not be in your solution.
-  (list 'incomplete)
+  
 )
-
