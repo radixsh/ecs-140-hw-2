92a1df86d13fa88c4c65dc1938553826c3c4c1fc
Radish <53844660+radixsh@users.noreply.github.com>
2024-05-05 14:27:51 -0700

    Write nfa.lisp with a findtrue() function

--- nfa/nfa.lisp
+++ nfa/nfa.lisp
@@ -1,25 +1,27 @@
+(defun findtrue (listoflists)
+    (cond
+        ((null listoflists)
+            nil
+        )
+        ((atom (car listoflists))
+            (or (not (null (car listoflists))) (findtrue (cdr listoflists)))
+        )
+    )
+
+)
+
 (defun reachable (transition start final input)
     (cond
         ((null input)
             (eql start final)
         )
         (t
-            (let (listtoflatten 
-                (carmap
-                    (lambda (onestate)
-                        (reachable transition onestate final (cdr input))
-                    )
-                    (funcall transition start (car input))
-                ))
-
-                (funcall
-                    (lambda (thing)
-                        (print thing)
-                    ) 
-                    listtoflatten
+            (findtrue (mapcar
+                (lambda (onestate)
+                    (reachable transition onestate final (cdr input))
                 )
-            )
-            
+                (funcall transition start (car input))
+            ))
         )
     )
 )
